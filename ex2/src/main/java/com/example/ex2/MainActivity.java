d231d1f7e3976e95742892d8eff1d953c5b390b2
omerpo904 <oporat88@gmail.com>
2016-09-27 17:27:46 +0300

    27/9

--- ex2/src/main/java/com/example/ex2/MainActivity.java
+++ ex2/src/main/java/com/example/ex2/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.ex2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int clickCount=0;
 
     @Override
     protected void onRestart() {
@@ -42,10 +48,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i(MyApp.MYTAG,"Main:onRestoreInstance event");
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.i(MyApp.MYTAG,"Main:onSaveInstance event");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
+    public void displaymessaage( View v){
+        clickCount++;
+       EditText et= (EditText)findViewById(R.id.eTxtpo);
+        String messeage =getResources().getString(R.string.emptyname);
+        String messeag=getResources().getString(R.string.name);
+        String name=et.getText().toString();
+
+            if(name.isEmpty())
+            Toast.makeText(this,String.format(messeage,clickCount),Toast.LENGTH_LONG).show();
+             else
+            Toast.makeText(this,String.format(messeag,clickCount,name),Toast.LENGTH_LONG).show();
+
+
+    }
+
 }
